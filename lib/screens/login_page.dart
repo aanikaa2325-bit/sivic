15f4c89716bd441613cdaf601c29e2fd4a4bc551
mdanwarulazim <anwarulazim.professional@gmail.com>
2026-08-06 05:40:22 +0600

    App label updated

---
+++ lib/screens/login_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/login_bg.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
