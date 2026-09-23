96273dbee72fa72a99aaa89f9baa5397cac5e57d
mdanwarulazim <anwarulazim.professional@gmail.com>
2026-09-23 22:23:46 +0600

    modified profile.dart

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
 
 class Profile extends StatefulWidget {
   const Profile({super.key});
@@ -7,211 +11,257 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  String username = '';
+  String phone = '';
+  String email = '';
+  String nid = '';
 
-  String username = 'Anwarul Azim';
-  String countryCode = '+88';
-  String number = '01345678910';
+  bool isLoading = true;
 
-  String get phone => '$countryCode $number';
+  @override
+  void initState() {
+    super.initState();
+    fetchUserData();
+  }
 
-  String email = 'anwarulazim@support.sivic.com';
-  String nid = '012 345 678';
+  Future<void> fetchUserData() async {
+    try {
+      User? currentUser = FirebaseAuth.instance.currentUser;
+
+      if (currentUser != null) {
+        DocumentSnapshot userDoc = await FirebaseFirestore.instance
+            .collection('users')
+            .doc(currentUser.uid)
+            .get();
+
+        if (userDoc.exists) {
+          Map<String, dynamic> data = userDoc.data() as Map<String, dynamic>;
+
+          setState(() {
+            String firstName = data['firstName'] ?? '';
+            String lastName = data['lastName'] ?? '';
+            username = '$firstName $lastName'.trim();
+
+            phone = data['phone'] ?? 'No phone number';
+            email = data['email'] ?? 'No email';
+            nid = data['nid'] ?? 'No NID';
+
+            isLoading = false;
+          });
+        } else {
+          setState(() => isLoading = false);
+        }
+      } else {
+        setState(() => isLoading = false);
+      }
+    } catch (e) {
+      print("Error fetching data: $e");
+      setState(() => isLoading = false);
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color(0xFFF5F6F5),
-        appBar: AppBar(
-          surfaceTintColor: Colors.transparent,
-          backgroundColor: Colors.transparent,
-          toolbarHeight: 80,
-          leading: IconButton(
-            onPressed: (){
-              Navigator.pop(context);
-            },
-            icon: Image.asset(
-              'assets/icons/back.png',
-              height: 48,
-              width: 48,
-            ),
-            padding: const EdgeInsets.only(left: 16),
+      backgroundColor: const Color(0xFFF5F6F5),
+      appBar: AppBar(
+        surfaceTintColor: Colors.transparent,
+        backgroundColor: Colors.transparent,
+        toolbarHeight: 80,
+        leading: IconButton(
+          onPressed: (){
+            Navigator.pop(context);
+          },
+          icon: Image.asset(
+            'assets/icons/back.png',
+            height: 48,
+            width: 48,
           ),
-          title: const Text('Profile',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.w600,
-              color: Color(0xFF1E241E),
-            ),
+          padding: const EdgeInsets.only(left: 16),
+        ),
+        title: const Text('Profile',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+            color: Color(0xFF1E241E),
           ),
         ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 18.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                SizedBox(height: 24),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      height: 72,
-                      width: 72,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(18),
-                        image: DecorationImage(
-                          image: AssetImage('assets/images/user_icon.png'),
-                          fit: BoxFit.fitHeight,
-                          alignment: Alignment.centerLeft,
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 12),
-                    Text(username,
-                      style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.w600,
-                        color: Color(0xFF1E241E),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 22),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      height: 36,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        color: Colors.white,
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Image(image:AssetImage(
-                                'assets/icons/password.png'),
-                              height: 16,
-                              width: 16,
-                            ),
-                            SizedBox(width: 8),
-                            Text('Change password',
-                              style: TextStyle(
-                                fontSize: 14,
-                                fontWeight: FontWeight.w500,
-                                color: Color(0xFF333733),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    SizedBox(width: 8),
-                    Container(
-                      height: 36,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        color: Color(0xFF1C1F1C),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Image(image:AssetImage(
-                                'assets/icons/edit.png'),
-                              height: 16,
-                              width: 16,
-                            ),
-                            SizedBox(width: 8),
-                            Text('Edit profile',
-                              style: TextStyle(
-                                fontSize: 14,
-                                fontWeight: FontWeight.w500,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 40),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Phone',
-                      style: TextStyle(
-                        fontSize: 13,
-                        fontWeight: FontWeight.w500,
-                        color: Color(0xFF333733),
-                      ),
-                    ),
-                    SizedBox(height: 8),
-                    Text(phone,
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w400,
-                        color: Color(0xFF333733),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 16),
-                Container(height: 1.25, color: Color(0xFFE4E8E4)),
-                SizedBox(height: 20),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Email',
-                      style: TextStyle(
-                        fontSize: 13,
-                        fontWeight: FontWeight.w500,
-                        color: Color(0xFF333733),
-                      ),
-                    ),
-                    SizedBox(height: 8),
-                    Text(email,
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w400,
-                        color: Color(0xFF333733),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 16),
-                Container(height: 1.25, color: Color(0xFFE4E8E4)),
-                SizedBox(height: 20),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('National ID',
-                      style: TextStyle(
-                        fontSize: 13,
-                        fontWeight: FontWeight.w500,
-                        color: Color(0xFF333733),
-                      ),
-                    ),
-                    SizedBox(height: 8),
-                    Text(nid,
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w400,
-                        color: Color(0xFF333733),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 16),
-              ],
+      ),
+      body: isLoading
+          ? const Center(child:
+            SpinKitFadingFour(
+              color: Color(0xFF85B650),
+              size: 50.0,
             ),
+          )
+          : SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 18.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const SizedBox(height: 24),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 72,
+                    width: 72,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(18),
+                      image: const DecorationImage(
+                        image: AssetImage('assets/images/user_icon.png'),
+                        fit: BoxFit.fitHeight,
+                        alignment: Alignment.centerLeft,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  Text(username,
+                    style: const TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w600,
+                      color: Color(0xFF1E241E),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 22),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 36,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Colors.white,
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Image(image:AssetImage(
+                              'assets/icons/password.png'),
+                            height: 16,
+                            width: 16,
+                          ),
+                          SizedBox(width: 8),
+                          Text('Change password',
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                              color: Color(0xFF333733),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  Container(
+                    height: 36,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: const Color(0xFF1C1F1C),
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Image(image:AssetImage(
+                              'assets/icons/edit.png'),
+                            height: 16,
+                            width: 16,
+                          ),
+                          SizedBox(width: 8),
+                          Text('Edit profile',
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 40),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text('Phone',
+                    style: TextStyle(
+                      fontSize: 13,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xFF333733),
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(phone, // Uses the fetched phone
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                      color: Color(0xFF333733),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+              Container(height: 1.25, color: const Color(0xFFE4E8E4)),
+              const SizedBox(height: 20),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text('Email',
+                    style: TextStyle(
+                      fontSize: 13,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xFF333733),
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(email,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                      color: Color(0xFF333733),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+              Container(height: 1.25, color: const Color(0xFFE4E8E4)),
+              const SizedBox(height: 20),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text('National ID',
+                    style: TextStyle(
+                      fontSize: 13,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xFF333733),
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(nid,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                      color: Color(0xFF333733),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+            ],
           ),
         ),
+      ),
     );
   }
 }
-
