07c137ef7e5891687120f0eef86676b566707e31
mdanwarulazim <anwarulazim.professional@gmail.com>
2026-09-23 20:39:13 +0600

    added auth_gate.dart

---
+++ lib/auth_gate.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'package:sivic/screens/login_page.dart';
+import 'package:sivic/navigation_menu.dart';
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+
+        if (snapshot.hasData) {
+          return NavigationMenu();
+        }
+
+        return LoginPage();
+      },
+    );
+  }
+}
